6b95322df39d77a3bcf4eedebe593404f5129d8c
dochira480-cell <dochira480@gmail.com>
2026-01-07 15:15:36 +1100

    print

---
+++ Print statements .py
@@ -0,0 +1,10 @@
+# print a sentence
+print("hello world!")
+# print a number
+print (42)
+# next line after print
+print ("this is a new line\n")
+#pass multiple arguments for printing
+print ("hello",5)
+#end argument of print sentence
+print ("welcome to", end ="*")
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("hadi")
+print("what colour is the sky?")
+print("a.blue\nb.green\nc.red\nd.yellow")
+answer = input ("enter your answer:")
+if answer == "a":
+    print("you are correct!")
+else:
+    print ("you are wrong!")
+    
