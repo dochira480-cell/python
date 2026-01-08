6dd641b1325733218e136ca6ae112e5da6b84edc
dochira480-cell <dochira480@gmail.com>
2026-01-08 13:56:23 +1100

    birthday homework

---
+++ Birthdays project.py
@@ -0,0 +1,13 @@
+print("Birthdays of 5 people:")
+print()
+#storing birthdays in variables
+a="28/02/2003"
+print ("Bob =", a)
+b="15/06/2002"
+print ("Ryder =", b)
+c= "01/01/2013"
+print ("Ben =", c)
+d= "23/09/2021"
+print ("Alex =", d)
+e= "30/11/2017"
+print ("Mason =", e)
