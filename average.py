5e997fce672b1aa9614b661c3d1e3d8972d0884f
dochira480-cell <dochira480@gmail.com>
2026-01-13 15:59:05 +1100

    average

---
+++ average.py
@@ -0,0 +1,9 @@
+# storing values
+rock1 = 10
+paper1 = 15
+scissors1 = 20
+Hadi1 = 567
+Hadi2 = 23456789
+# calculating average
+average = (rock1 + paper1 + scissors1 + Hadi1 + Hadi2)/5
+print ("the average is :", average)
