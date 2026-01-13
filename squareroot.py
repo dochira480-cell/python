9299c65ef742d81bac013fc57d8a0a3e640390b7
dochira480-cell <dochira480@gmail.com>
2026-01-13 16:28:20 +1100

    square

---
+++ squareroot.py
@@ -0,0 +1,9 @@
+num = 56
+num_sqrt = num**0.5
+print(f'The Number is {num} and it is stored in num_sqrt {num_sqrt}')
+# Python Program to calculate the square root
+
+num = int(input("enter the number:"))
+
+num_sqrt = num**0.5
+print("the square root of", num, "is", num_sqrt)
