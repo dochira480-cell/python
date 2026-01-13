657d6e2d6b54c7bc4811274049d1d844c54153aa
dochira480-cell <dochira480@gmail.com>
2026-01-13 15:05:21 +1100

    homework

---
+++ Congratulations.py
@@ -0,0 +1,16 @@
+# Convert a string into Upper Case
+x = "coNgRaLaTioNS"
+print(str.lower(x))
+
+print(str.upper(x))
+
+# Take two strings as input from user
+print("Enter the First String: ")
+strOne = input()
+
+print("Enter the Second String: ")
+strTwo = input()
+# Concatenate the two strings
+# Using Addition Operator
+strThree = strOne + " " + strTwo
+print("\nConcatenated String: ", strThree)
---
+++ stuff.py
@@ -0,0 +1,19 @@
+hello = "This is a statement."
+print(hello)
+# print last two letters of this statement
+print(hello[-2:])
+# print first four letter of this statement
+print(hello[:4])
+print (hello[2])
+print (hello[:8])
+print (len(hello))
+print (hello[len(hello)-1])
+x = 38.5
+print(type(x))
+if x > 100:
+    print("x is greater that 5")
+else:
+    print ("no")
+print(x > 100)
+print (x < 100)
+print (x == 100)
