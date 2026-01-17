09b63086612fc5aeb4b52555f4a0c3b99ee928a0
dochira480-cell <dochira480@gmail.com>
2026-01-17 15:56:26 +1100

    link

---
+++ link.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number: "))
+if (i < 15):
+    print(i, "is less than 15")
+else:
+    print(i, "is greater than or equal to 15")  
+    print("This is inside else block")
+print("This is outside the else block")
+
