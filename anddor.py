be5e9be8f7497ff63afed0f17b7358b33c927447
dochira480-cell <dochira480@gmail.com>
2026-01-19 19:40:44 +1100

    hi

---
+++ anddor.py
@@ -0,0 +1,11 @@
+a = 10
+b = -10
+c = 0
+if a and b and c:
+    print ("all values are have boolean true value")
+else:
+    print ("at least one value has boolean false value")
+    if a > 0 or b > 0 or c > 0:
+        print ("at least one value is positive")    
+    else:
+        print ("no value is positive")
