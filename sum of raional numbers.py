74b5721fb6e7c46457f8319ee408b6ddf4cbd156
dochira480-cell <dochira480@gmail.com>
2026-02-17 18:22:28 +1100

    i will run

--- sum of raional numbers.py
+++ sum of raional numbers.py
@@ -4,7 +4,7 @@ n = int(input("Enter the number of terms: "))
 sum = 0 # initialize sum
 i = 1 # initialize counter
 while i <= n:
-    sum = sum + 1/i # add the current term to the sum
+    sum = sum + i # add the current term to the sum
     i = i + 1 # increment the counter
 
 print ("\n sum = ", sum) # print the final sum
