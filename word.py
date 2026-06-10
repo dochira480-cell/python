14d1af920e1f01acadca9a2137ca0a8c478b0494
dochira480-cell <dochira480@gmail.com>
2026-06-10 19:35:05 +1000

    fds

---
+++ word.py
@@ -0,0 +1,12 @@
+def match_words(words):
+    ctr = 0
+    lst = []
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            ctr += 1
+            lst.append(word)
+    print("list of words that start and end with the same letter: ", lst)
+    return ctr
+
+count = match_words(['abc', 'xyz', 'aba', '1221'])
+print("Number of words that start and end with the same letter: ", count)
